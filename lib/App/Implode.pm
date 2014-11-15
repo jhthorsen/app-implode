2d528e337e8d81c9a7c728dd5ad20ef1d44baffd
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-11-15 11:09:09 +0100

    Add support for custom APP_EXPLODE_DIR

--- lib/App/Implode.pm
+++ lib/App/Implode.pm
@@ -22,8 +22,33 @@ L<cpanfile|Module::CPANfile>. Example C<cpanfile>:
 
 =head1 SYNOPSIS
 
+=head2 Generetor
+
   $ cd my-project
   $ implode myapp.pl out.pl
+
+=head2 Consumer
+
+It is possible to set environment variables on the consumer side to instruct
+how the code will be "exploded".
+
+  $ out.pl
+  $ APP_EXPLODE_VERBOSE=1 out.pl
+  $ APP_EXPLODE_DIR=/extract/files/here out.pl
+
+=over 4
+
+=item * APP_EXPLODE_VERBOSE
+
+Set this to a true value to get debug output.
+
+=item * APP_EXPLODE_DIR
+
+The default is to put the extracted files in a default
+L<tmpdir|File::Spec/tmpdir>. A custom C<APP_EXPLODE_DIR> can be specified
+if to override that behavior.
+
+=back
 
 =head1 CAVEAT
 
